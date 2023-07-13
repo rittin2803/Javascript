19ea8f84f931098135008654ac7a7b4ce738b837
rittin <rittin280@gmail.com>
2023-07-13 23:49:33 +0530

    Javascript Basics

--- JsBasics/Javascript.js
+++ JsBasics/Javascript.js
@@ -3,3 +3,42 @@ console.log('Hello world');
 // Variables 
 let vname = 'Rittin';
 console.log(vname);
+
+//Constants - For not change value of variable.
+const salary = 67;
+
+//Datatype - "Primitive"
+// String, Number, Boolean, Undef, NULL
+
+//Datatype - "Reference"
+// Object, Array, Function
+
+//DYNAMIC LANGUAGE
+
+//Objects
+let fname = 'Rittin';
+let age = 19;
+let person = {
+    //Object literal
+    fname: 'Mosh',
+    age: 19
+};
+
+//2 ways to work with objects
+//DOT NOTATION
+person.fname = 'Mithra';
+console.log(person.fname);
+
+//BRACKET NOTATION
+person['name'] = 'Rittin Mithra';
+
+//Arrays
+let selectedColors = ['red', 'blue'];
+
+//Functions
+function greet(fname){
+    //Logic here
+    console.log('Hello' + fname);
+
+}
+greet('Rittin');
